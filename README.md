d94dbd80010da16f9ef0dcd2fedbe0c52d1ffd4b
eduardoserpa1 <dudu.rserpa@gmail.com>
2022-09-22 00:43:15 -0300

    cleaning the code and adding the terminal version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# TrilhaDoOuro
+#  Dynamic programming with ternary trees
+### _Finding the highest sum path in nxn matrices starting from the lower left cell to the upper right cell_
+
+
+### **Solutions**
+- Recursive without optimization 
+- Recursive optimized
+- Non-recursive optimized
--- src/Ouro.java
+++ src/Ouro.java
@@ -1,38 +1,194 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Queue;
 
 public class Ouro {
-
-    public static HashMap<String,Integer> memoria = new HashMap<>();
-    public static HashMap<String,String> memoria_path = new HashMap<>();
-    public static String[][] memoria_path_nao_recursivo;
-    static int menor_int = Integer.MIN_VALUE/20;
+    /*
+    *   José Eduardo Rodrigues Serpa - 20200311-7
+    *   Turma 031 - 2022/2
+    */
+    static HashMap<String,Integer> otimizacao = new HashMap<>();
+    static HashMap<String,String> caminho_recursivo = new HashMap<>();
+    static String[][] caminho_nao_recursivo;
+    static int menor_int = Integer.MIN_VALUE/100;
 
     public static void main(String[] args) throws IOException{
+        if(args.length == 1){
+            String[][] mapa = arq(args[0]);
+            determina_menor_inteiro(mapa);
+
+            long tempoFinal = 0;
+            long tempoInicial = 0;
+
+            tempoInicial = System.currentTimeMillis();
+            System.out.println("Resultado do algoritmo recursivo com otimização exibindo a quantidade de ouro e melhor rota:\n");
+            algoritmo_recursivo_otimizado(mapa);
+            tempoFinal = System.currentTimeMillis();
+            System.out.println("\nt: " + (tempoFinal-tempoInicial) + " ms\n");
 
 
-        String[][] mapa = arq("teste_enunciado.txt");
+            tempoInicial = System.currentTimeMillis();
+            System.out.println("Resultado do algoritmo NÃO recursivo com otimização exibindo a quantidade de ouro e melhor rota:\n");
+            algoritmo_nao_recursivo_otimizado(mapa);
+            tempoFinal = System.currentTimeMillis();
+            System.out.println("\nt: " + (tempoFinal-tempoInicial) + " ms\n");
 
-        long tempoFinal = System.currentTimeMillis();
-        long tempoInicial = System.currentTimeMillis();
+            tempoInicial = System.currentTimeMillis();
+            System.out.println("Resultado do algoritmo recursivo SEM otimização exibindo a quantidade de ouro apenas:\n");
+            algoritmo_recursivo_nao_otimizado(mapa);
+            tempoFinal = System.currentTimeMillis();
+            System.out.println("\nt: " + (tempoFinal-tempoInicial) + " ms\n");
 
-
-        int r1 = procura_ouro_mem(mapa);
-        System.out.println(r1);
-
-        System.out.println();
-        int r2 = procura_ouro_nao_recursivo(mapa);
-        System.out.println(r2);
-
-
-
+        }
     }
-    public static String monta_caminho_nao_recursivo(String[][] m){
+
+    /*
+    *   Chamadas das funções desenvolvidas
+    */
+
+    static void algoritmo_recursivo_nao_otimizado(String[][] m){
+        System.out.println(ouro_recursivo_nao_otimizado(m,m.length-1,0));
+    }
+
+    static void algoritmo_recursivo_otimizado(String[][] m){
+        System.out.println(ouro_recursivo_otimizado(m,m.length-1,0));
+        System.out.println(monta_caminho_algoritmo_recursivo(caminho_recursivo,m.length-1));
+    }
+
+    static void algoritmo_nao_recursivo_otimizado(String[][] m){
+        System.out.println(ouro_nao_recursivo(m));
+        System.out.println(monta_caminho_nao_recursivo(caminho_nao_recursivo));
+    }
+
+
+    /*
+    *   Algoritmos desenvolvidos
+    */
+
+    static int ouro_recursivo_nao_otimizado(String[][] matriz, int linha, int coluna){
+        int n = matriz.length - 1;
+
+        int atual = 0;
+
+        atual = toInt(matriz[linha][coluna]);
+
+        if(linha == 0 && coluna == n)
+            return atual;
+        if(coluna == n)
+            return atual + ouro_recursivo_nao_otimizado(matriz,linha-1,coluna);
+        if(linha == 0)
+            return atual + ouro_recursivo_nao_otimizado(matriz,linha,coluna+1);
+
+        String norte = String.valueOf(ouro_recursivo_nao_otimizado(matriz,linha-1,coluna));
+        String leste = String.valueOf(ouro_recursivo_nao_otimizado(matriz,linha,coluna+1));
+        String nordeste = String.valueOf(ouro_recursivo_nao_otimizado(matriz,linha-1, coluna+1));
+
+        int melhor_escolha = strMax ( String.valueOf(strMax( norte, leste)) , nordeste);
+
+        return atual + melhor_escolha;
+    }
+
+    static int ouro_recursivo_otimizado(String[][] matriz, int linha, int coluna){
+        String label = linha+","+coluna;
+
+
+        int n = matriz.length - 1;
+
+        int atual = 0;
+
+        atual = toInt(matriz[linha][coluna]);
+
+        if(linha == 0 && coluna == n){
+            return atual;
+        }
+
+        if (otimizacao.containsKey(label))
+            return otimizacao.get(label);
+
+
+        if(coluna == n){
+            caminho_recursivo.put(label,"N");
+            return atual + ouro_recursivo_otimizado(matriz,linha-1,coluna);
+        }
+
+        if(linha == 0){
+            caminho_recursivo.put(label,"E");
+            return atual + ouro_recursivo_otimizado(matriz,linha,coluna+1);
+        }
+
+        String norte = String.valueOf(ouro_recursivo_otimizado(matriz,linha-1,coluna));
+        String leste = String.valueOf(ouro_recursivo_otimizado(matriz,linha,coluna+1));
+        String nordeste = String.valueOf(ouro_recursivo_otimizado(matriz,linha-1, coluna+1));
+
+        int index = retorna_index_maior ( norte, nordeste, leste);
+        int melhor_escolha = 0;
+
+        if (index == 1){
+            melhor_escolha = toInt(norte);
+            caminho_recursivo.put(label,"N");
+        }else
+        if (index == 2){
+            melhor_escolha = toInt(nordeste);
+            caminho_recursivo.put(label,"NE");
+        }else
+        if (index == 3){
+            melhor_escolha = toInt(leste);
+            caminho_recursivo.put(label,"E");
+        }
+
+        otimizacao.put(label,atual + melhor_escolha);
+
+
+        return atual + melhor_escolha;
+    }
+
+    static String ouro_nao_recursivo(String[][] matriz) {
+
+        int mlen = matriz.length;
+
+        String [][]clone = new String[mlen + 1][mlen + 1];
+
+        for (int i = 0; i < mlen; i++)
+            for (int j = 0; j <= mlen; j++)
+                if (j==0)
+                    clone[i][j] = Integer.toString(menor_int);
+                else
+                    clone[i][j] = "0";
+
+        for (int i = 0; i <= mlen; i++)
+            clone[mlen][i] = Integer.toString(menor_int);
+
+        for (int i = mlen-1; i >= 0; i--)
+            for (int j = 1; j <= mlen; j++)
+                if(i==mlen-1 && j == 1)
+                    clone[i][j] = matriz[i][j-1];
+                else
+                    clone[i][j] = Integer.toString( strMax(Integer.toString(strMax(clone[i + 1][j], clone[i][j - 1])), clone[i + 1][j - 1]) + toInt(matriz[i][j - 1]));
+
+        caminho_nao_recursivo = clone;
+
+        return clone[0][mlen];
+    }
+
+
+    /*
+    * Funções utilitarias :)
+    */
+
+    static void determina_menor_inteiro(String[][] m){
+        int menor = Integer.MAX_VALUE;
+
+        for (int i = 0; i < m.length; i++)
+            for (int j = 0; j < m.length; j++)
+                if (!m[i][j].equals("x"))
+                    if (toInt(m[i][j]) < menor)
+                        menor = toInt(m[i][j]);
+
+        menor_int = menor-1;
+    }
+
+    static String monta_caminho_nao_recursivo(String[][] m){
         String r = "";
 
         int linha_origem = m.length-2;
@@ -58,7 +214,7 @@ public class Ouro {
                 sul = m[linha+1][coluna];
             }
 
-            int index = strMaxS(sul,sudoeste,oeste);
+            int index = retorna_index_maior(sul,sudoeste,oeste);
 
             switch (index){
                 case 1:
@@ -89,111 +245,8 @@ public class Ouro {
 
         return r_reverse;
     }
-    public static int procura_ouro_nao_recursivo(String[][] m){
-        int response = toInt(ouro_nao_recursivo(m));
-        System.out.println("melhor caminho: "+monta_caminho_nao_recursivo(memoria_path_nao_recursivo));
-        return response;
-    }
 
-    public static int procura_ouro_mem(String[][] m){
-        int response = ouro_mem(m,m.length-1,0);
-        System.out.println("melhor caminho: "+monta_caminho(memoria_path,m.length-1));
-        return response;
-    }
-    public static int procura_ouro(String[][] m){
-        return ouro(m,m.length-1,0);
-    }
-
-
-    public static String ouro_nao_recursivo(String[][] grid_str) {
-
-        int N = grid_str.length;
-
-        String [][]sum = new String[N + 1][N + 1];
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j <= N; j++) {
-                if (j==0)
-                    sum[i][j] = Integer.toString(menor_int);
-                else
-                    sum[i][j] = "0";
-            }
-        }
-        for (int i = 0; i <= N; i++) {
-            sum[N][i] = Integer.toString(menor_int);
-        }
-
-        for (int i = N-1; i >= 0; i--) {
-            for (int j = 1; j <= N; j++) {
-                if(i==N-1 && j == 1)
-                    sum[i][j] = grid_str[i][j-1];
-                else
-                    sum[i][j] = Integer.toString( strMax(Integer.toString(strMax(sum[i + 1][j], sum[i][j - 1])), sum[i + 1][j - 1]) + toInt(grid_str[i][j - 1]));
-            }
-        }
-
-        memoria_path_nao_recursivo = sum;
-
-        return sum[0][N];
-    }
-
-
-    public static int ouro_mem(String[][] matriz, int linha, int coluna){
-        String label = linha+","+coluna;
-
-
-        int n = matriz.length - 1;
-
-        int atual = 0;
-
-        atual = toInt(matriz[linha][coluna]);
-
-        if(linha == 0 && coluna == n){
-            return atual;
-        }
-
-        if (memoria.containsKey(label)){
-            return memoria.get(label);
-        }
-
-
-        if(coluna == n){
-            memoria_path.put(label,"N");
-            return atual + ouro_mem(matriz,linha-1,coluna);
-        }
-
-        if(linha == 0){
-            memoria_path.put(label,"E");
-            return atual + ouro_mem(matriz,linha,coluna+1);
-        }
-
-        String norte = String.valueOf(ouro_mem(matriz,linha-1,coluna));
-        String leste = String.valueOf(ouro_mem(matriz,linha,coluna+1));
-        String nordeste = String.valueOf(ouro_mem(matriz,linha-1, coluna+1));
-
-        int index = strMaxS ( norte, nordeste, leste);
-        int melhor_escolha = 0;
-
-        if (index == 1){
-            melhor_escolha = toInt(norte);
-            memoria_path.put(label,"N");
-        }else
-        if (index == 2){
-            melhor_escolha = toInt(nordeste);
-            memoria_path.put(label,"NE");
-        }else
-        if (index == 3){
-            melhor_escolha = toInt(leste);
-            memoria_path.put(label,"E");
-        }
-
-        memoria.put(label,atual + melhor_escolha);
-
-
-        return atual + melhor_escolha;
-    }
-
-    public static String monta_caminho(HashMap<String,String> coord, int n){
+    static String monta_caminho_algoritmo_recursivo(HashMap<String,String> coord, int n){
         String r = "";
 
         int linha = n;
@@ -209,49 +262,23 @@ public class Ouro {
             switch (coord_atual){
                 case "N":
                     linha--;
-                break;
+                    break;
                 case "NE":
                     linha--;
                     coluna++;
-                break;
+                    break;
                 case "E":
                     coluna++;
-                break;
+                    break;
 
                 default:
-                break;
+                    break;
             }
         }
         return r;
     }
 
-    public static int ouro(String[][] matriz, int linha, int coluna){
-        int n = matriz.length - 1;
-
-        int atual = 0;
-
-        atual = toInt(matriz[linha][coluna]);
-
-        if(linha == 0 && coluna == n)
-            return atual;
-        if(coluna == n)
-            return atual + ouro(matriz,linha-1,coluna);
-        if(linha == 0)
-            return atual + ouro(matriz,linha,coluna+1);
-
-        String norte = String.valueOf(ouro(matriz,linha-1,coluna));
-        String leste = String.valueOf(ouro(matriz,linha,coluna+1));
-        String nordeste = String.valueOf(ouro(matriz,linha-1, coluna+1));
-
-        int melhor_escolha = strMax ( String.valueOf(strMax( norte, leste)) , nordeste);
-
-        //  verificar qual a melhor escolha entre (norte, nordeste e leste), e determinar o passo que
-        //  foi dado antes da recursão
-
-        return atual + melhor_escolha;
-    }
-
-    public static int toInt(String x){
+    static int toInt(String x){
         if (x.length()>1)
             if ( !(x.charAt(0) > 47 && x.charAt(0) < 58) )
                 return Integer.parseInt(x.substring(1)) * -1;
@@ -262,7 +289,7 @@ public class Ouro {
             return Integer.parseInt(x);
     }
 
-    public static int strMaxS(String a, String b, String c){
+    static int retorna_index_maior(String a, String b, String c){
         if(a.equals("x") && b.equals("x") && c.equals("x"))
             return 2;
         if (a.equals("x") && b.equals("x"))
@@ -271,7 +298,6 @@ public class Ouro {
             return 2;
         if (b.equals("x") && c.equals("x"))
             return 1;
-
 
         int aa = toInt(a);
         int bb = toInt(b);
@@ -286,7 +312,8 @@ public class Ouro {
 
         return 0;
     }
-    public static int strMax(String a, String b){
+
+    static int strMax(String a, String b){
         if(a.equals("x") && b.equals("x"))
             return menor_int;
         if (a.equals("x"))
@@ -303,9 +330,9 @@ public class Ouro {
             return bb;
     }
 
-    public static String[][] arq(String file_name) throws IOException {
+    static String[][] arq(String file_name) throws IOException {
 
-        BufferedReader buffer = new BufferedReader(new FileReader("./src/"+file_name));
+        BufferedReader buffer = new BufferedReader(new FileReader(file_name));
         String linha = "";
         int len = Integer.parseInt(buffer.readLine());
         String[][] matriz = new String[len][len];
